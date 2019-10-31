361b30787001178dce29d4e128c9a9384c70afd3
yikdata2018 <1432467203@qq.com>
2019-10-31 23:48:21 +0800

    flask

---
+++ study_space/demo_flask/app01/app_main.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import render_template
+from flask import url_for
+from flask import request
+from flask import redirect
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    return redirect(url_for('Add'))
+
+@app.route('/Add', methods=['GET','POST'])
+def Add():
+    if request.method == 'POST':
+        a = request.form['adder1']
+        b = request.form['adder2']
+        a = int(a)
+        b = int(b)
+        c = a + b
+        return render_template('index.html', message=str(c))
+    return render_template('index.html')
+
+@app.route('/echart01', methods=['GET','POST'])
+# @auth.route('/echart01', methods=['GET','POST'])
+def Echart01():
+    return render_template('html_echart_001.html')
+
+@app.route('/echart02', methods=['GET','POST'])
+# @auth.route('/echart02', methods=['GET','POST'])
+def Echart02():
+    return render_template('html_echart_002.html')
+
+@app.route('/echart03', methods=['GET','POST'])
+# @auth.route('/echart03', methods=['GET','POST'])
+def Echart03():
+    return render_template('html_echart_003.html')
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=8080)
