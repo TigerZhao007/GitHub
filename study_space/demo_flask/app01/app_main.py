787d05d1d63bb26b435e2dac0125e0dfa0ef68c1
yikdata2018 <1432467203@qq.com>
2019-11-01 17:17:33 +0800

    flask

--- study_space/demo_flask/app01/app_main.py
+++ study_space/demo_flask/app01/app_main.py
@@ -36,5 +36,21 @@ def Echart02():
 def Echart03():
     return render_template('html_echart_003.html')
 
+@app.route('/web01', methods=['GET','POST'])
+# @auth.route('/echart03', methods=['GET','POST'])
+def web01():
+    return render_template('web01.html', title='测试', message=url_for('Add'))
+
+# @app.route('/Add2', methods=['GET','POST'])
+# def Add():
+#     if request.method == 'POST':
+#         a = request.form['adder1']
+#         b = request.form['adder2']
+#         a = int(a)
+#         b = int(b)
+#         c = a + b
+#         return render_template('web01.html', message=str(c))
+#     return render_template('web01.html')
+
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8081)
