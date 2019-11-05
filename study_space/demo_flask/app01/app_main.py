716348f8abde930632c6deaa89589a7d0a2cc51b
yikdata2018 <1432467203@qq.com>
2019-11-06 00:22:20 +0800

    修改完善flask, 后端向前端传递数据，只能传递数字，不能传递中文，后续可调整

--- study_space/demo_flask/app01/app_main.py
+++ study_space/demo_flask/app01/app_main.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import url_for
 from flask import request
 from flask import redirect
+import json
 
 app = Flask(__name__)
 
@@ -19,30 +20,34 @@ def Add():
         b = int(b)
         c = a + b
         return render_template('index.html', message=str(c))
-    else:
-        return render_template('index.html', message=str('请输入a&b数值'))
     return render_template('index.html')
 
 @app.route('/echart01', methods=['GET','POST'])
-# @auth.route('/echart01', methods=['GET','POST'])
 def Echart01():
+    # return render_template('html_echart_001.html')
+
     aa = ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
     xiaoliang = [5,20,36,10,10,20]
     chengben = [5,20,36,10,10,20]
-    return render_template('html_echart_001.html', aa=aa, xiaoliang=xiaoliang, chengben=chengben)
+    return render_template('html_echart_001.html', aa=aa, xiaoliang=xiaoliang, chengben=chengben, ensure_ascii=False)
+
+@app.route('/echart01_data')
+def get_data():
+    aa = ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+    xiaoliang = [5,20,36,10,10,20]
+    chengben = [5,20,36,10,10,20]
+    return json.dumps({'aa':aa, 'xiaoliang':xiaoliang, 'chengben':chengben},
+                      ensure_ascii=False)    #如果有中文的话，就需要ensure_ascii=False
 
 @app.route('/echart02', methods=['GET','POST'])
-# @auth.route('/echart02', methods=['GET','POST'])
 def Echart02():
     return render_template('html_echart_002.html')
 
 @app.route('/echart03', methods=['GET','POST'])
-# @auth.route('/echart03', methods=['GET','POST'])
 def Echart03():
     return render_template('html_echart_003.html')
 
 @app.route('/web01', methods=['GET','POST'])
-# @auth.route('/echart03', methods=['GET','POST'])
 def web01():
     return render_template('web01.html', title='测试', message=url_for('Add'))
 
@@ -56,4 +61,6 @@ def error(e):
     return render_template('index.html'), 500
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=8089)
+    app.run(host="0.0.0.0", port=8081)
+    # context = ('../zhengshu/1_bundle.crt', '../zhengshu/2_key.key')
+    # app.run(host="0.0.0.0", port=8080, threaded=True, ssl_context=context)
