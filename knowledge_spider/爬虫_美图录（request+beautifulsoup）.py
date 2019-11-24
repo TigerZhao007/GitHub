d48d4fd71b3c013c05042ddc51da9a4342642023
yikdata2018 <1432467203@qq.com>
2019-11-25 00:56:00 +0800

    美图录图片下载代码编写，后续需要添加批量下载主函数

--- knowledge_spider/爬虫_美图录（request+beautifulsoup）.py
+++ knowledge_spider/爬虫_美图录（request+beautifulsoup）.py
@@ -18,12 +18,23 @@ def getHtmlText(url):
     headers['User-Agent'] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) " \
                             "Chrome/63.0.3239.132 Safari/537." + str(random.randint(1, 99))
     # headers['Referer'] = '''https://www.meitulu.com/t/nvshen/'''
+    # headers['Accept'] = '''text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3'''
+    # headers['Accept-Encoding'] = '''gzip, deflate, br'''
+    # headers['Accept-Language'] = '''zh-CN,zh;q=0.9'''
     # headers['Host'] = '''www.meitulu.com'''
+    # headers['Cache-Control'] = '''max-age=0'''
+    # headers['Connection'] = '''keep-alive'''
+    # headers['If-None-Match'] = '''"5dd5e305-de19"'''
+    # headers['Sec-Fetch-Mode'] = '''navigate'''
+    # headers['Sec-Fetch-Site'] = '''none'''
+    # headers['Sec-Fetch-User'] = '''?1'''
+    # headers['Upgrade-Insecure-Requests'] = '''1'''
     # headers['Cookie'] = '''UM_distinctid=16dc50e174c278-024a8d7adaa2b7-7373e61-144000-16dc50e174d1a8; CNZZDATA1255487232=939500139-1570975242-%7C1570975242; CNZZDATA1255357127=771937645-1570965867-https%253A%252F%252Fwww.baidu.com%252F%7C1574603813'''
 
     # 读取HTML文本~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     try:
-        r = requests.get(url, headers, timeout=10)  # 如果状态码不是200 则应发HTTOError异常
+        # r = requests.get(url, headers, timeout=10)  # 如果状态码不是200 则应发HTTOError异常
+        r = requests.get(url, timeout=10)
         r.raise_for_status()               # 设置正确的编码方式
         r.encoding = r.apparent_encoding
         return r.text
@@ -58,6 +69,61 @@ def getImgList(html):
 
     return imglist
 
+# 图片详情页最大页码~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def getPicNum(html_pic):
+    ''' # url:网页地址; # return:返回网页数据 '''
+
+    # 导入所需模块~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    import pandas as pd
+    from bs4 import BeautifulSoup as bs
+
+    # 导入图片列表~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    soup = bs(html_pic, 'lxml')
+    max_page = soup.find('div', attrs={'id': 'pages'}).find_all('a')[-2].text
+
+    return max_page
+
+# 图片详情页列表~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def getPicList(html_pic):
+    ''' # url:网页地址; # return:返回网页数据 '''
+
+    # 导入所需模块~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    import pandas as pd
+    from bs4 import BeautifulSoup as bs
+
+    # 导入图片列表~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    soup = bs(html_pic, 'lxml')
+    soup = soup.find('div', attrs={'class': 'content'}).find_all('img')
+    pic_list = [x['src'] for x in soup]
+    pic_name = [x['alt'] for x in soup]
+    pic_dict = {'pic_list':pic_list, 'pic_name':pic_name}
+
+    return pic_dict
+
+# 图片详情页列表~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def downloadPic(path, pic_url, pic_name):
+
+    '''
+    :param path: # path = r'D:\Desktop'
+    :param pic_url: # pic_url = 'https://mtl.gzhuibei.com/images/img/17827/4.jpg'
+    :param pic_name: # pic_name = 'a'
+    :return: 返回网页数据
+    '''
+
+    # 导入所需模块~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    import requests
+
+    # 导入图片列表~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    r = requests.get(pic_url).content
+    path = path + '\\' + pic_name + '.jpg'
+
+    try:
+        with open(path, 'wb') as f:
+            f.write(r)
+            print('保存成功 %s.jpg' % pic_name)
+    except:
+        print('保存失败')
+
 # 爬虫代码汇总~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def main(url, tablename):
     import sqlalchemy
@@ -75,10 +141,10 @@ if __name__ == '__main__':
     import time
     t1 = time.time()
 
-    # url_list = ['https://www.meitulu.com/t/nvshen/'] + \
-    #            ['https://www.meitulu.com/t/nvshen/%s.html' %(x) for x in range(2, 38)]
+    url_list = ['https://www.meitulu.com/t/nvshen/'] + \
+               ['https://www.meitulu.com/t/nvshen/%s.html' %(x) for x in range(2, 38)]
 
-    url_list = ['https://www.meitulu.com/t/nvshen/%s.html' %(x) for x in range(2, 38)]
+    # url_list = ['https://www.meitulu.com/t/nvshen/%s.html' %(x) for x in range(2, 38)]
     for url in url_list:
         main(url, tablename='meitulu_single')
 
