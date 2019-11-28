e4c88cc7038765f6c7bb7fe0f58ee34381e32ca0
yikdata2018 <1432467203@qq.com>
2019-11-28 09:18:25 +0800

    添加美图录中图片下载文件，添加小说信息下载清单。数据均以写入数据库

---
+++ knowledge_spider/爬虫_yssn.xyz小说.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+时间：2019-10-13
+作者: zuoshao（佐少）
+代码说明：目标地址https://www.meitulu.com/，美图录
+"""
+
+# 请求网页数据~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def getHtmlText(url):
+    ''' # url:网页地址; # return:返回网页数据 '''
+
+    # 导入所需模块~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    import requests
+    import random
+
+    # 设置表头~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) " \
+                            "Chrome/63.0.3239.132 Safari/537." + str(random.randint(1, 99))
+
+    # 读取HTML文本~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    try:
+        # r = requests.get(url, headers, timeout=10)  # 如果状态码不是200 则应发HTTOError异常
+        r = requests.get(url, timeout=10)
+        r.raise_for_status()               # 设置正确的编码方式
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return "Something Wrong!"
+
+# 指定某一页，统计该页信息~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def getImgList(html):
+
+    # 导入所需模块~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    import pandas as pd
+    from bs4 import BeautifulSoup as bs
+
+    # 导入图片列表~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    soup = bs(html, 'lxml')
+    soup = soup.find('div', attrs={'class': 'posts-loop clear'})
+    soup1 = soup.find_all('h2', attrs={'class': 'entry-title'})    # 表示在整个网页中过滤出所有图片的地址，放在imglist中
+    soup2 = soup.find_all('span', attrs={'class': 'entry-category'})    # 表示在整个网页中过滤出所有图片的地址，放在imglist中
+
+    # 统计列表信息~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    story_url = [x.find('a')['href'] for x in soup1]
+    story_name = [x.text for x in soup1]
+    story_type = [x.text.replace('\n','') for x in soup2]
+
+    story_list = pd.DataFrame({'story_name':story_name, 'story_type':story_type, 'story_url': story_url})
+
+    return story_list
+
+# 指定某一页，统计该页信息~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def main(url):
+
+    import sqlalchemy
+    import pandas as pd
+
+    engine = sqlalchemy.create_engine("postgresql://postgres:123456@47.100.173.196:5432/project_spider",
+                                      pool_size=20, max_overflow=5)
+
+    html = getHtmlText(url)
+    story_info = getImgList(html)
+    story_info['is_download'] = 'flase'
+
+    # 获取指定页图片列表~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    try:
+        sql = ''' SELECT story_url FROM public.story_info '''
+        with engine.connect() as conn:
+            story_list_in = tuple(list(pd.read_sql_query(sql, conn)['pic_url']))
+
+        story_info = story_info[~story_info['pic_url'].isin(story_list_in)]
+    except:
+        pass
+
+    with engine.connect() as conn:
+        story_info.to_sql('story_info', conn, if_exists='append', index=False)
+
+# 主函数~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if __name__ == '__main__':
+
+    import time
+    t1 = time.time()
+    list_false = []
+
+    url_list = ['http://yssn.xyz/'] + ['http://yssn.xyz/index.php/page/%s/'%(x) for x in range(1, 200)]
+    # url = 'http://yssn.xyz/index.php/page/2/'
+    for url in url_list:
+        try:
+            print('正在处理页码：%s' % (url))
+            main(url)
+            time.time(1)
+        except:
+            list_false = list_false + [url]
+
+    t2 = time.time()
+    print("总耗时：%.2f 秒" % (t2 - t1))
