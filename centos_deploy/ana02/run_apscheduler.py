95f912fc98c6207910d808795ced09bdbd7eec40
yikdata2018 <1432467203@qq.com>
2019-12-10 12:07:17 +0800

    centos deploy ana02

--- centos_deploy/ana02/run_apscheduler.py
+++ centos_deploy/ana02/run_apscheduler.py
@@ -9,7 +9,7 @@ import sys
 
 # 导入需要运行的自定义模块~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 path = r'D:/Work/GitHub/centos_deploy/ana02/'
-path = r'/root/sharefile/ana02'
+path = r'/root/GitHub/centos_deploy/ana02/'
 
 # sys.path.append(os.path.dirname(os.path.abspath('__file__')))
 sys.path.append(os.path.dirname(path))
